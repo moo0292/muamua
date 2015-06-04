0e5345ecbed9db45a089f84de62d66162842fa59
Kittitat Rodchaidee <moo0292@Kittitats-Mac-Pro.local>
2015-06-03 18:16:02 -0700

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,125 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
+
+.run(function($ionicPlatform, $rootScope, $state) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+
+
+    });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+    })
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.hot', {
+        url: '/hot',
+        views: {
+            'hot': {
+                templateUrl: 'templates/hot.html',
+                controller: 'HomeCtrl'
+            }
+        }
+    })
+
+    .state('tab.peek', {
+            url: '/peek',
+            views: {
+                'peek': {
+                    templateUrl: 'templates/peek.html',
+                    controller: 'PeekCtrl'
+                }
+            }
+        })
+        .state('tab.peek-detail', {
+            url: '/peek/detail',
+            views: {
+                'peek': {
+                    templateUrl: 'templates/peek-detail.html',
+                    controller: 'PeekCtrl'
+                }
+            }
+        })
+        .state('tab.peek-detail-message', {
+            url: '/peek/detail/message',
+            views: {
+                'peek': {
+                    templateUrl: 'templates/peek-detail-message.html',
+                    controller: 'PeekCtrl'
+                }
+            }
+        })
+        .state('tab.home', {
+            url: '/home',
+            views: {
+                'home': {
+                    templateUrl: 'templates/home.html',
+                    controller: 'HomeCtrl'
+                }
+            }
+        })
+        .state('tab.home-detail', {
+            url: '/home/detail',
+            views: {
+                'home': {
+                    templateUrl: 'templates/message.html',
+                    controller: 'HomeCtrl'
+                }
+            }
+        })
+        .state('tab.profile', {
+            url: '/profile',
+            views: {
+                'profile': {
+                    templateUrl: 'templates/profile.html',
+                    controller: 'ProfileCtrl'
+                }
+            }
+        })
+        .state('tab.profile-detail', {
+            url: '/profile/detail',
+            views: {
+                'profile': {
+                    templateUrl: 'templates/profile-detail.html',
+                    controller: 'ProfileCtrl'
+                }
+            }
+        })
+        .state('tab.write', {
+            url: '/write',
+            views: {
+                'write': {
+                    templateUrl: 'templates/write.html',
+                    controller: 'WriteCtrl'
+                }
+            }
+        });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/home');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,423 @@
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function($ionicPopup, $ionicPlatform, $http, $rootScope, $scope, $location, $cordovaGeolocation, $cordovaDevice, $ionicLoading) {
+
+        $scope.allMessages = [];
+        $scope.checked = true;
+        $scope.currentState = "New";
+        $scope.currentFrNumber = 0;
+
+        $scope.goToSpecific = function(frontNum) {
+            $location.path('/tab/home/detail');
+            $rootScope.selectedNumber = frontNum;
+            $rootScope.selectedMessage = $scope.allMessages[frontNum]
+        };
+
+        $scope.voteUp = function(arrayIndex, messageId) {
+            $scope.allMessages[arrayIndex].canUpVote = false;
+            $scope.allMessages[arrayIndex].canDownVote = false;
+            $scope.allMessages[arrayIndex].rating++;
+
+            $http.put("http://10.0.1.181:7000/upvote/" + $rootScope.uuid + "/" + messageId)
+                .success(function(data) {
+
+                }, function(err) {
+                    alert("Unable to upvote message");
+                });
+        };
+        $scope.voteDown = function(arrayIndex, messageId) {
+            $scope.allMessages[arrayIndex].canUpVote = false;
+            $scope.allMessages[arrayIndex].canDownVote = false;
+            $scope.allMessages[arrayIndex].rating--;
+
+            $http.put("http://10.0.1.181:7000/downvote/" + $rootScope.uuid + "/" + messageId)
+                .success(function(data) {
+
+                }, function(err) {
+
+                    alert("Unable to downote message");
+                });
+        };
+
+        $scope.voteUpSpec = function(arrayIndex, messageId) {
+            $scope.allMessages[arrayIndex].canUpVote = false;
+            $scope.allMessages[arrayIndex].canDownVote = false;
+            $scope.allMessages[arrayIndex].rating++;
+
+            $rootScope.selectedMessage.canUpVote = false;
+            $rootScope.selectedMessage.canDownVote = false;
+            $rootScope.selectedMessage.rating++;
+
+            $http.put("http://10.0.1.181:7000/upvote/" + $rootScope.uuid + "/" + messageId)
+                .success(function(data) {
+
+                }, function(err) {
+                    alert("Unable to upvote message");
+                });
+        };
+
+        $scope.voteDownSpec = function(arrayIndex, messageId) {
+            $scope.allMessages[arrayIndex].canUpVote = false;
+            $scope.allMessages[arrayIndex].canDownVote = false;
+            $scope.allMessages[arrayIndex].rating--;
+
+            $rootScope.selectedMessage.canUpVote = false;
+            $rootScope.selectedMessage.canDownVote = false;
+            $rootScope.selectedMessage.rating--;
+
+            $http.put("http://10.0.1.181:7000/downvote/" + $rootScope.uuid + "/" + messageId)
+                .success(function(data) {
+
+                }, function(err) {
+
+                    alert("Unable to downote message");
+                });
+        };
+
+        $scope.doRefresh = function() {
+
+            $ionicLoading.show({
+                template: 'Loading...'
+            });
+
+            $http.get("http://10.0.1.181:7000/get_all/" + $rootScope.longt + "/" + $rootScope.lat)
+                .success(function(data) {
+                    $scope.allMessages = data;
+                    for (var i = 0; i < $scope.allMessages.length; i++) {
+                        //do an underscore contain to check if a user can still vote
+                        $scope.allMessages[i].frontNumber = i;
+                        // console.log($scope.allMessages[i].voters);
+                        if (_.contains($scope.allMessages[i].voters, $rootScope.uuid)) {
+                            $scope.allMessages[i].canUpVote = false;
+                            $scope.allMessages[i].canDownVote = false;
+                        } else {
+                            $scope.allMessages[i].canUpVote = true;
+                            $scope.allMessages[i].canDownVote = true;
+                        }
+                    }
+                    $ionicLoading.hide();
+                    $scope.$broadcast('scroll.refreshComplete');
+                })
+        };
+
+        $scope.shouldShow = function() {
+            return false;
+        };
+
+        $scope.changeToNew = function() {
+
+            $ionicLoading.show({
+                template: 'Loading...'
+            });
+
+            $http.get("http://10.0.1.181:7000/get_all/" + $rootScope.longt + "/" + $rootScope.lat)
+                .success(function(data) {
+                    $scope.allMessages = data;
+                    for (var i = 0; i < $scope.allMessages.length; i++) {
+                        //do an underscore contain to check if a user can still vote
+                        $scope.allMessages[i].frontNumber = i;
+                        // console.log($scope.allMessages[i].voters);
+                        if (_.contains($scope.allMessages[i].voters, $rootScope.uuid)) {
+                            $scope.allMessages[i].canUpVote = false;
+                            $scope.allMessages[i].canDownVote = false;
+                        } else {
+                            $scope.allMessages[i].canUpVote = true;
+                            $scope.allMessages[i].canDownVote = true;
+                        }
+                    }
+                    $ionicLoading.hide();
+                    $scope.$broadcast('scroll.refreshComplete');
+                })
+        };
+
+        $scope.changeToHot = function() {
+
+            $ionicLoading.show({
+                template: 'Loading...'
+            });
+
+            $http.get("http://10.0.1.181:7000/highest_rated/" + $rootScope.longt + "/" + $rootScope.lat)
+                .success(function(data) {
+
+                    $scope.allMessages = data;
+
+                    for (var i = 0; i < $scope.allMessages.length; i++) {
+                        //do an underscore contain to check if a user can still vote
+                        $scope.allMessages[i].frontNumber = i;
+                        // console.log($scope.allMessages[i].voters);
+                        if (_.contains($scope.allMessages[i].voters, $rootScope.uuid)) {
+                            $scope.allMessages[i].canUpVote = false;
+                            $scope.allMessages[i].canDownVote = false;
+                        } else {
+                            $scope.allMessages[i].canUpVote = true;
+                            $scope.allMessages[i].canDownVote = true;
+                        }
+                    }
+                    $ionicLoading.hide();
+                })
+        };
+
+        $scope.reply_text = {};
+        $scope.reply_object = {};
+
+        $scope.submitReply = function() {
+
+            $scope.reply_text.message_id = $rootScope.selectedMessage._id;
+            $scope.reply_text.user_id = $rootScope.uuid;
+
+            var confirmPopup = $ionicPopup.confirm({
+                title: 'Confirm',
+                template: 'Are you sure to post message?'
+            });
+
+            confirmPopup.then(function(res) {
+                if (res) {
+                    $ionicLoading.show({
+                        template: 'Loading...'
+                    });
+                    $http.post("http://10.0.1.181:7000/post_reply", $scope.reply_text)
+                        .success(function(data) {
+                            //you also have to do a post to add the comment onto a person's array
+                            $ionicLoading.hide();
+                            $rootScope.selectedMessage.comment.push($scope.reply_text.answer);
+                            $scope.reply_text = {};
+                        }, function(err) {
+                            $ionicLoading.hide();
+                            alert("Unable to post message");
+                        });
+                } else {
+
+                }
+            });
+
+        };
+
+        $ionicPlatform.ready(function() {
+
+            $rootScope.uuid = $cordovaDevice.getUUID();
+
+            var posOptions = {
+                timeout: 10000,
+                enableHighAccuracy: false
+            };
+
+
+            $cordovaGeolocation
+                .getCurrentPosition(posOptions)
+                .then(function(position) {
+                    $rootScope.lat = position.coords.latitude
+                    $rootScope.longt = position.coords.longitude
+
+                    $ionicLoading.show({
+                        template: 'Loading...'
+                    });
+
+                    $http.get("http://10.0.1.181:7000/find_user/" + $rootScope.uuid)
+                        .success(function(user) {
+                            $rootScope.user_id = user._id;
+                            $rootScope.user_posts = user.posts;
+                        })
+
+                    $http.get("http://10.0.1.181:7000/get_all/" + $rootScope.longt + "/" + $rootScope.lat)
+                        .success(function(data) {
+                            $scope.allMessages = data;
+                            for (var i = 0; i < $scope.allMessages.length; i++) {
+                                //do an underscore contain to check if a user can still vote
+                                $scope.allMessages[i].frontNumber = i;
+                                // console.log($scope.allMessages[i].voters);
+                                if (_.contains($scope.allMessages[i].voters, $rootScope.uuid)) {
+                                    $scope.allMessages[i].canUpVote = false;
+                                    $scope.allMessages[i].canDownVote = false;
+                                } else {
+                                    $scope.allMessages[i].canUpVote = true;
+                                    $scope.allMessages[i].canDownVote = true;
+                                }
+                            }
+
+                            $ionicLoading.hide();
+                        })
+                        .error(function(data) {
+                            alert("Unable to connect to the server. Check your internet connection");
+                        });
+
+                }, function(err) {
+                    alert("Cannot get current user location");
+                });
+
+        });
+
+    })
+    .controller('PeekCtrl', function($ionicPopup, $ionicPlatform, $http, $rootScope, $scope, $location, $cordovaGeolocation, $cordovaDevice, $ionicLoading) {
+
+
+        $scope.goToPeek = function() {
+            $location.path('/tab/peek/detail');
+        }
+        $scope.goToPeekDetail = function(peekTitle, latitude, longitude) {
+
+            $rootScope.peek_title = peekTitle;
+
+            //pass in the latitude and longitude
+            //get the highest rated of each university
+
+            $ionicLoading.show({
+                template: 'Loading...'
+            });
+
+            $http.get("http://10.0.1.181:7000/school_rated/" + longitude + "/" + latitude)
+                .success(function(data) {
+
+                    $rootScope.peekArray = [];
+                    //set the peek array
+                    $rootScope.peekArray = data;
+
+                    //go to the peek page
+                    $ionicLoading.hide();
+                    $location.path('/tab/peek/detail');
+                })
+        };
+
+        $scope.goPeekDetailMessage = function(input_index) {
+            $rootScope.selectedPeekMessage = $rootScope.peekArray[input_index];
+            $location.path('/tab/peek/detail/message');
+        };
+
+        $scope.reply_text = {};
+        $scope.reply_object = {};
+
+        $scope.submitReply = function() {
+
+            $scope.reply_text.message_id = $rootScope.selectedPeekMessage._id;
+
+            console.log($scope.reply_text);
+            var confirmPopup = $ionicPopup.confirm({
+                title: 'Confirm',
+                template: 'Are you sure to post message?'
+            });
+
+            confirmPopup.then(function(res) {
+                if (res) {
+                    $ionicLoading.show({
+                        template: 'Loading...'
+                    });
+                    $http.post("http://10.0.1.181:7000/post_reply", $scope.reply_text)
+                        .success(function(data) {
+                            //you also have to do a post to add the comment onto a person's array
+                            $ionicLoading.hide();
+                            $rootScope.selectedPeekMessage.comment.push($scope.reply_text.answer);
+                            $scope.reply_text = {};
+                        }, function(err) {
+                            $ionicLoading.hide();
+                            alert("Unable to post message");
+                        });
+                } else {
+
+                }
+            });
+
+        };
+    })
+    .controller('ProfileCtrl', function($http, $scope, $location, $rootScope, $ionicPopup, $ionicLoading) {
+        $scope.goToSpecific = function(frontNum) {
+            $location.path('/tab/profile/detail');
+            $rootScope.selectedNumber = frontNum;
+            $rootScope.selectedMessage = $rootScope.user_posts[frontNum];
+            console.log(frontNum);
+            console.log($rootScope.selectedMessage);
+        };
+
+        $scope.reply_text = {};
+        $scope.reply_object = {};
+
+        $scope.submitReply = function() {
+
+            $scope.reply_text.message_id = $rootScope.selectedMessage._id;
+
+            console.log($scope.reply_text);
+            var confirmPopup = $ionicPopup.confirm({
+                title: 'Confirm',
+                template: 'Are you sure to post message?'
+            });
+
+            confirmPopup.then(function(res) {
+                if (res) {
+                    $ionicLoading.show({
+                        template: 'Loading...'
+                    });
+                    $http.post("http://10.0.1.181:7000/post_reply", $scope.reply_text)
+                        .success(function(data) {
+                            //you also have to do a post to add the comment onto a person's array
+                            $ionicLoading.hide();
+                            $rootScope.selectedMessage.comment.push($scope.reply_text.answer);
+                            $scope.reply_text = {};
+                        }, function(err) {
+                            $ionicLoading.hide();
+                            alert("Unable to post message");
+                        })
+                        .error(function(data) {
+                            alert("Unable to connect to the server. Check your internet connection");
+                        });
+                } else {
+
+                }
+            });
+
+        };
+
+        $scope.refresh = function() {
+            $http.get("http://10.0.1.181:7000/find_user/" + $rootScope.uuid)
+                .success(function(user) {
+                    $rootScope.user_posts = user.posts;
+                })
+
+            $scope.$broadcast('scroll.refreshComplete');
+
+        };
+
+
+    })
+    .controller('MessageCtrl', function($scope) {
+        $scope.reply_text = "";
+
+        $scope.submitReply = function() {
+            console.log("Submit Reply");
+        }
+    })
+    .controller('WriteCtrl', function($ionicPopup, $http, $scope, $rootScope, $ionicLoading) {
+        $scope.body_text = {};
+
+
+        $scope.writeMessage = function() {
+
+            $scope.object = {};
+            $scope.object.title = $scope.body_text.title;
+            $scope.object.latitude = $rootScope.lat;
+            $scope.object.longtitude = $rootScope.longt;
+            $scope.object.text = $scope.body_text.body;
+            $scope.object.owner = $rootScope.user_id;
+
+            var confirmPopup = $ionicPopup.confirm({
+                title: 'Confirm',
+                template: 'Are you sure to post message?'
+            });
+            confirmPopup.then(function(res) {
+                if (res) {
+                    $ionicLoading.show({
+                        template: 'Loading...'
+                    });
+                    $http.post("http://10.0.1.181:7000/post_message", $scope.object)
+                        .success(function(data) {
+                            //you also have to do a post to add the comment onto a person's array
+                            $ionicLoading.hide();
+                            $scope.body_text = {};
+                            alert("successfully post message");
+                        }, function(err) {
+                            $ionicLoading.hide();
+                            alert("Unable to post message");
+                        });
+                } else {
+
+                }
+            });
+        }
+    });
